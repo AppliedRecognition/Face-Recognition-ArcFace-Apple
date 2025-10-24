237b74dd2ffe6ba83aad3ed5904d5999f34231a5
Jakub Dolejs <jakubdolejs@gmail.com>
2025-10-24 14:47:36 +0100

    Added tests to test memory pressure under repeated use

--- Sources/TestSupport/TestSupport.swift
+++ Sources/TestSupport/TestSupport.swift
@@ -21,55 +21,33 @@ public class TestSupportResources {
     public static let bundle: Bundle = .module
     
     public func faceAndImageForSubject(_ subject: String) async throws -> (Face, Image) {
-        let image = try await self.imageForSubject(subject)
+        let image = try self.imageForSubject(subject)
         let face = try await self.faceInImage(image)
         return (face, image)
     }
     
     public func faceForSubject(_ subject: String) async throws -> Face {
-        let image = try await self.imageForSubject(subject)
+        let image = try self.imageForSubject(subject)
         guard let face = try await self.faceDetection.detectFacesInImage(image, limit: 1).first else {
             throw TestSupportError("Failed to detect a face in image \(subject).jpg")
         }
         return face
     }
     
-    public func imageForSubject(_ subject: String) async throws -> Image {
-        guard let url = Bundle.module.url(forResource: subject, withExtension: "jpg") else {
-            throw TestSupportError("Failed to get a URL for \(subject).jpg")
+    public func imageForSubject(_ subject: String) throws -> Image {
+        return try autoreleasepool {
+            guard let url = Bundle.module.url(forResource: subject, withExtension: "jpg") else {
+                throw TestSupportError("Failed to get a URL for \(subject).jpg")
+            }
+            let data = try Data(contentsOf: url)
+            guard let uiImage = UIImage(data: data) else {
+                throw TestSupportError("Failed to decode image data")
+            }
+            guard let image = Image(uiImage: uiImage) else {
+                throw TestSupportError("Failed to convert UIImage to Image")
+            }
+            return image
         }
-        let data = try Data(contentsOf: url)
-        guard let uiImage = UIImage(data: data) else {
-            throw TestSupportError("Failed to decode image data")
-        }
-        let orientation: CGImagePropertyOrientation
-        switch uiImage.imageOrientation {
-        case .up:
-            orientation = .up
-        case .down:
-            orientation = .down
-        case .left:
-            orientation = .left
-        case .right:
-            orientation = .right
-        case .upMirrored:
-            orientation = .upMirrored
-        case .downMirrored:
-            orientation = .downMirrored
-        case .leftMirrored:
-            orientation = .leftMirrored
-        case .rightMirrored:
-            orientation = .rightMirrored
-        default:
-            orientation = .up
-        }
-        guard let cgImage = uiImage.cgImage else {
-            throw TestSupportError("Failed to convert image to CGImage")
-        }
-        guard let image = Image(cgImage: cgImage, orientation: orientation) else {
-            throw TestSupportError("Failed to convert CGImage to Ver-ID image")
-        }
-        return image
     }
     
     public func faceTemplateForSubject(_ subject: String) -> [Float]? {
--- Tests/FaceRecognitionArcFaceCoreTests/FaceRecognitionArcFaceCoreTests.swift
+++ Tests/FaceRecognitionArcFaceCoreTests/FaceRecognitionArcFaceCoreTests.swift
@@ -28,4 +28,24 @@ final class FaceRecognitionArcFaceCoreTests: XCTestCase {
             self.add(attachment)
         }
     }
+    
+    func testDetectFaceRepeatedly() async throws {
+        throw XCTSkip("For testing memory pressure")
+        for _ in 0..<1000 {
+            for name in ["subject1-01", "subject1-02", "subject2-01"] {
+                let image = try self.testResources.imageForSubject(name)
+                _ = try await self.testResources.faceDetection.detectFacesInImage(image, limit: 1)
+            }
+        }
+    }
+    
+    func testAlignFaceRepeatedly() async throws {
+        throw XCTSkip("For testing memory pressure")
+        for _ in 0..<1000 {
+            for name in ["subject1-01", "subject1-02", "subject2-01"] {
+                let (face, image) = try await self.testResources.faceAndImageForSubject(name)
+                _ = try FaceAlignment.alignFace(face, image: image)
+            }
+        }
+    }
 }
